20474d2683478630733b3a6d7d8977b990af5a06
MCW <mcw@simcorp.com>
2014-09-15 11:56:16 +0100

    removed unused namespace

--- RoslynOnFly/CsProject.fs
+++ RoslynOnFly/CsProject.fs
@@ -1,35 +1,32 @@
-﻿namespace RoslynOnFly
+﻿module CsProject
+open FSharp.Data
 
-module CsProject =
+type ProjectXml = XmlProvider<"projectExample2.xml">
+type Path = string
 
-    open FSharp.Data
+type Reference =
+    | RefFile of Path
+    | RefProject of Path
 
-    type ProjectXml = XmlProvider<"projectExample2.xml">
-    type Path = string
+type ProjectFile = 
+    | CsFile of Path
+    | EmbeddedRes of Path
 
-    type Reference =
-        | RefFile of Path
-        | RefProject of Path
-
-    type ProjectFile = 
-        | CsFile of Path
-        | EmbeddedRes of Path
-
-    type Project = {name: string; references: seq<Reference>; files: seq<ProjectFile>}
+type Project = {name: string; references: seq<Reference>; files: seq<ProjectFile>}
 
 
-    let getProject (filePath: Path) =
-        let proj = ProjectXml.Load filePath
+let getProject (filePath: Path) =
+    let proj = ProjectXml.Load filePath
 
-        let refs = proj.ItemGroups |> Seq.collect (fun p->seq{
-            for reference in p.ProjectReferences -> RefProject reference.Include
-            for reference in p.References -> RefFile reference.Include
-        })
+    let refs = proj.ItemGroups |> Seq.collect (fun p->seq{
+        for reference in p.ProjectReferences -> RefProject reference.Include
+        for reference in p.References -> RefFile reference.Include
+    })
     
-        let files = proj.ItemGroups |> Seq.collect (fun p->seq{
-            for file in p.Compiles -> CsFile(file.Include)
-            for file in p.EmbeddedResources -> EmbeddedRes(file.Include)
-        })
+    let files = proj.ItemGroups |> Seq.collect (fun p->seq{
+        for file in p.Compiles -> CsFile(file.Include)
+        for file in p.EmbeddedResources -> EmbeddedRes(file.Include)
+    })
 
-        let n=(proj.PropertyGroups |> Seq.find (fun x-> x.AssemblyName.IsSome )).AssemblyName.Value
-        {name = n; references = refs; files = files}
+    let n=(proj.PropertyGroups |> Seq.find (fun x-> x.AssemblyName.IsSome )).AssemblyName.Value
+    {name = n; references = refs; files = files}
